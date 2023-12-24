66bf3716373fad43311d16ec87bd1e11a4055758
valzzu <52419539+valzzu@users.noreply.github.com>
2023-12-24 21:00:55 +0200

    player movement and changed zombie model
    when i changed zombie model i broke da AI sorry :(

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float speed = 5f; // Adjust the speed as needed
+    [SerializeField] float rotationSpeed = 100f; // Adjust the rotation speed as needed   
     public static Player Instance;
     
     // Start is called before the first frame update
@@ -15,9 +17,18 @@ public class Player : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get input from the arrow keys or joystick
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate movement and rotation
+        Vector3 movement = speed * Time.deltaTime * new Vector3(0f, 0f, verticalInput);
+        float rotation = horizontalInput * rotationSpeed * Time.deltaTime;
+
+        // Move and rotate the car
+        transform.Translate(movement);
+        transform.Rotate(0f, rotation, 0f);
     }
 }
